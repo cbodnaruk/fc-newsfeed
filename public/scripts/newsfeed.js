f6aa34b012de7e27227a29921c6987cce806cd6e
Tristan Cliff <mrcliff59@gmail.com>
2024-08-26 00:11:51 +1000

    Add loading spinner, refactor to footer

--- public/scripts/newsfeed.js
+++ public/scripts/newsfeed.js
@@ -1,17 +1,18 @@
- 	
- 
- setInterval(reloadPosts,60000);
-    $( document ).ready(function() {
-        reloadPosts();
-    });
 
-  function reloadPosts(){
-    $("#newsfeed").load("../"+dash_id+"/newsfeed/postsload/",function(response, status, xhr){
-        if (xhr.status == 200){
-            document.getElementById("newsfeed").children[1].style.border = "4px solid #ffffff"
-        } else if (xhr.status == 304){
-            document.getElementById("newsfeed").children[1].style.border = "4px solid #c0c0c0"
+
+setInterval(reloadPosts, 60000);
+$(document).ready(function () {
+    reloadPosts();
+});
+
+function reloadPosts() {
+    $("#loading").show()
+    $("#newsfeed").load("../" + dash_id + "/newsfeed/postsload/", function (response, status, xhr) {
+        if (xhr.status == 200) {
+            document.getElementById("postlist").children[0].style.border = "2px solid #ffffff"
+        } else if (xhr.status == 304) {
+            document.getElementById("postlist").children[0].style.border = "2px solid #c0c0c0"
         }
+        $("#loading").hide()
     });
-
-  }
+}
