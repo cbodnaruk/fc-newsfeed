da976ff4455485ae046c557352ee8e6e15f429b2
Tristan Cliff <mrcliff59@gmail.com>
2024-08-26 21:40:30 +1000

    Main work done - final scrolling needing fix

--- public/scripts/newsfeed.js
+++ public/scripts/newsfeed.js
@@ -9,9 +9,9 @@ function reloadPosts() {
     $("#loading").show()
     $("#newsfeed").load("../" + dash_id + "/newsfeed/postsload/", function (response, status, xhr) {
         if (xhr.status == 200) {
-            document.getElementById("postlist").children[0].style.border = "2px solid #ffffff"
+            document.getElementById("postlist").children[0].classList.add("recentpost")
         } else if (xhr.status == 304) {
-            document.getElementById("postlist").children[0].style.border = "2px solid #c0c0c0"
+            document.getElementById("postlist").children[0].classList.remove("recentpost")
         }
         $("#loading").hide()
     });
