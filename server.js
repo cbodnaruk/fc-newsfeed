f663fbba368fa381624e147a0dc2ae5eb8c8d24b
Ben Finney <ben+ecmascript@benfinney.id.au>
2024-09-04 14:20:00 +1000

    Get the database connection URL from the application config object.

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 var fs = require('fs');
 var http = require('http');
-//var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-//var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-var dblog = fs.readFileSync('database_login.txt', 'utf8');
-
-
-
-//var credentials = {key: privateKey, cert: certificate};
+const config = require('./config.js');
 
 const express = require('express')
 const app = express()
 const pgp = require('pg-promise')();
-global.db = pgp(dblog);
+global.db = pgp(config.get('database.url'));
 const pug = require('pug');
 var httpServer = http.createServer(app);
 const expressWs = require('express-ws')(app);
