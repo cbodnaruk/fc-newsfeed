a8a7dc6da961cc535648e1eeb918e1c791329746
cbodnaruk <91246921+cbodnaruk@users.noreply.github.com>
2024-09-08 21:47:05 +1000

    add cue input html

--- public/scripts/audio.js
+++ public/scripts/audio.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     var audio_cues = $("#audio_cues")
     var cue_html = ""
     for (i in audioCues){
-        cue_html = `<div class="audio_cue" id="cue_${audioCues[i].id}"><`
-        audio_cues.append()
+        cue_html = `<div class="audio_cue" id="cue_${audioCues[i].id}"><input type="text" id="cue_name_${audioCues[i].id} class="audio_input" value="${audioCues[i].name}><input type="text" id="cue_url_${audioCues[i].id} class="audio_input" value="${audioCues[i].url}></div>`
+        audio_cues.append(cue_html)
     }
 });
