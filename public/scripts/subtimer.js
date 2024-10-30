93830bf7849ab56a2cc8394bfb86f82a6e65ae09
cbodnaruk <91246921+cbodnaruk@users.noreply.github.com>
2024-10-30 17:09:39 +1100

    Added audio engine partial, finished features

--- public/scripts/subtimer.js
+++ public/scripts/subtimer.js
@@ -1,46 +1,64 @@
 console.log("loaded")
-
+var interval
 const subtimer = {
     time: 0,
 
-    last_tick:0,
-    interval:0,
-    length:0,
-    tick: function(){
+    last_tick: 0,
+    interval: 0,
+    length: 0,
+    tick: function () {
         this.tick.bind(this)
         let s = Math.round(Date.now() / 1000);
         if (s > this.last_tick) {
-                this.time += 1;
-                updateClock(this.time)
-                if (this.time == this.length){
-                    this.timeout()
-                }
-            };
+            this.time += 1;
+            updateClock(this.time)
+            if (this.time > this.length) {
+                this.timeout()
+            }
+        };
 
-    
+
         this.last_tick = s;
 
     },
-    start: function(t){
+    start: function (t) {
         let tick = this.tick.bind(this)
         this.length = t
-        this.interval = setInterval(tick,250)
+        interval = setInterval(tick, 250)
         $("#subtimer_start_button").hide('fast')
         $("#subtimer_view").show()
     },
-    stop: function(){
-        clearInterval(this.interval)
+    stop: function () {
+        clearInterval(interval)
+        $("#subtimer_time").text("00:00")
     },
-    timeout: function(){
+    timeout: function () {
+        if (prefs.subtimer_pass_fail) {
+            this.fail()
+        } else {
+            var audio = document.getElementById("subtimer_fail_audio");
+            audio.play()
+            this.stop()
+        }
+    },
+    fail: function () {
+        //play fail audio
+        var audio = document.getElementById("subtimer_fail_audio");
+        audio.play()
+        this.stop()
+    },
+    pass: function () {
+        var audio = document.getElementById("subtimer_pass_audio");
+        audio.play()
 
+        this.stop()
     }
 
 }
 
 
 document.addEventListener('keydown', (event) => {
-    if (event.key == " "){
-        alert("space")
+    if (event.key == " ") {
     }
 });
 
@@ -51,9 +69,9 @@ function st_checkTime(i) {
     return i;
 }
 
-function updateClock(t){
-let rt = subtimer.length - t +1
-    let rmins = st_checkTime(Math.floor(rt/60))
+function updateClock(t) {
+    let rt = subtimer.length - t + 1
+    let rmins = st_checkTime(Math.floor(rt / 60))
     let rsecs = st_checkTime(rt % 60)
-$("#subtimer_time").text(`${rmins}:${rsecs}`)
+    $("#subtimer_time").text(`${rmins}:${rsecs}`)
 }
