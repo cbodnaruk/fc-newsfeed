b3f45ba7e86830c18a2cdbf0340c901060f547e0
cbodnaruk <91246921+cbodnaruk@users.noreply.github.com>
2025-01-20 19:17:53 +1100

    Automatically load last dash

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,7 +1,23 @@
-var input = document.getElementById("dashboard_id");
-input.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("gobtn").click();
+window.addEventListener("DOMContentLoaded", (event) => {
+    var input = document.getElementById("dashboard_id");
+    input.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            openDash();
+        }
+    })
+    });
+    if (localStorage.getItem("defaultDash")) {
+        window.location = localStorage.getItem("defaultDash")
     }
-}); 
+
+
+
+
+
+
+function openDash() {
+    dashId = $("#dashboard_id").val()
+    localStorage.setItem("defaultDash", dashId)
+    window.location = dashId
+}
